8f3f29ec40f93284d093b654aa490176d96265f2
Fillipe Galiza <fillipe.galiza@olist.com>
2019-04-07 19:56:25 -0300

    Add ChoiceViewSet, ChoiceSerializer and nested route for choice

--- quizzy/apps/quizzes/serializers.py
+++ quizzy/apps/quizzes/serializers.py
@@ -1,9 +1,21 @@
 from rest_framework import serializers
 
-from .models import Question
+from .models import Question, Choice
 
 
 class QuestionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Question
         fields = ('text',)
+
+
+class ChoiceSerializer(serializers.Serializer):
+    text = serializers.CharField(max_length=200)
+
+    def create(self, validated_data):
+        return Choice.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.text = validated_data.get('text', instance.text)
+        instance.save()
+        return instance
--- quizzy/apps/quizzes/views.py
+++ quizzy/apps/quizzes/views.py
@@ -1,9 +1,63 @@
-from rest_framework import viewsets
+from django.core.exceptions import ObjectDoesNotExist
+from rest_framework import status, viewsets
+from rest_framework.response import Response
 
-from .models import Question
-from .serializers import QuestionSerializer
+from .models import Choice, Question
+from .serializers import ChoiceSerializer, QuestionSerializer
 
 
 class QuestionViewSet(viewsets.ModelViewSet):
     queryset = Question.objects.all().order_by('-text')
     serializer_class = QuestionSerializer
+
+
+class ChoiceViewSet(viewsets.ModelViewSet):
+    queryset = Choice.objects.all().order_by('-question')
+    serializer_class = ChoiceSerializer
+
+    def create(self, request, *args, **kwargs):
+        question_id = kwargs['nested_1_pk']
+        text = request.data.get('text')
+
+        try:
+            question = Question.objects.get(id=question_id)
+        except ObjectDoesNotExist:
+            return Response(
+                'Question does not exist',
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        if not text:
+            return Response(
+                'Invalid choice text',
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        if question.choices.filter(text=text):
+            return Response(
+                'Choice text already registered for this question',
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        choice = {
+            'question': question_id,
+            'text': text
+        }
+
+        serializer = ChoiceSerializer(data=choice)
+
+        if not serializer.is_valid():
+            return Response(
+                'Invalid choice payload',
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        serializer.save(question_id=question_id)
+
+        headers = self.get_success_headers(serializer.data)
+
+        return Response(
+            serializer.data,
+            status=status.HTTP_201_CREATED,
+            headers=headers,
+        )
