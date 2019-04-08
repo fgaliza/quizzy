bef788df63d6993a91f6297202aa76bddf9eea01
Fillipe Galiza <fillipe.galiza@olist.com>
2019-04-07 21:37:19 -0300

    Add QuizViewSet with retrieve and list custom methods, serializers and tests

--- quizzy/apps/quizzes/routes.py
+++ quizzy/apps/quizzes/routes.py
@@ -1,6 +1,6 @@
 from rest_framework_nested import routers
 
-from .views import ChoiceViewSet, QuestionViewSet
+from .views import QuestionViewSet, ChoiceViewSet, QuizViewSet
 
 app_name = "quizzes"
 
@@ -10,6 +10,10 @@ question_router.register(r'questions', QuestionViewSet, basename='questions')
 choice_router = routers.NestedDefaultRouter(question_router, r'questions')
 choice_router.register(r'choices', ChoiceViewSet, basename='choices')
 
+quiz_router = routers.DefaultRouter()
+quiz_router.register(r'quiz', QuizViewSet, basename='quiz')
+
 urlpatterns = []
 urlpatterns += question_router.urls
 urlpatterns += choice_router.urls
+urlpatterns += quiz_router.urls
